ae3c83b8841190a1a159b4bbb4bec126a7761321
rimbo537 <izuravel524@gmail.com>
2023-05-21 15:58:38 +0300

    create data model

---
+++ lib/models/data_model/data_model.dart
@@ -0,0 +1,33 @@
+// import 'package:freezed_annotation/freezed_annotation.dart';
+
+// part 'data_model.freezed.dart';
+// part 'data_model.g.dart';
+
+// @freezed
+// class DataModel with _$DataModel {
+//   factory DataModel({
+//     required String? error,
+//     required List<TaskModel> data,
+//   }) = _DataModel;
+
+//   factory DataModel.fromJson(Map<String, dynamic> json) =>
+//       _$DataModelFromJson(json);
+// }
+
+// @freezed
+// class TaskModel with _$TaskModel {
+//   factory TaskModel({
+//     required String taskId,
+//     required int status,
+//     required String name,
+//     required int type,
+//     required String description,
+//     required String finishDate,
+//     required int urgent,
+//     required String syncTime,
+//     required String file,
+//   }) = _TaskModel;
+
+//   factory TaskModel.fromJson(Map<String, dynamic> json) =>
+//       _$TaskModelFromJson(json);
+// }
